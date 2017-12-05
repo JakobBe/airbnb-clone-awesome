dbc4fc7f0621d9917b86e50f303127d4a670169a
Yuzhu Hou <yuzhu.hou@gmail.com>
2017-12-05 11:31:34 +0100

    added routes, controllers and views for flats, users and bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
 
-  root to: 'pages#home'
+  root to: 'flats#index'
 
   devise_for :registrations, :controllers => { registrations: "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [ :edit, :update ]
+
+  resources :flats, only: [ :show, :new, :create ] do
+    resources :bookings, only: [ :new, :create, :update ]
+  end
+  resources :bookings, only: [ :index ]
+
+
+
 end
