5633abb4dd7291295da6ce4aae09cdb6efee069f
laura hallam <laura_hallam@yahoo.co.uk>
2017-12-07 17:11:10 +0100

    stash

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207145419) do
+ActiveRecord::Schema.define(version: 20171207153628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,17 @@ ActiveRecord::Schema.define(version: 20171207145419) do
     t.index ["reset_password_token"], name: "index_registrations_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string   "message"
+    t.integer  "user_id"
+    t.integer  "flat_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "rating"
+    t.index ["flat_id"], name: "index_reviews_on_flat_id", using: :btree
+    t.index ["user_id"], name: "index_reviews_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -86,5 +97,7 @@ ActiveRecord::Schema.define(version: 20171207145419) do
   add_foreign_key "bookings", "flats"
   add_foreign_key "bookings", "users"
   add_foreign_key "flats", "users"
+  add_foreign_key "reviews", "flats"
+  add_foreign_key "reviews", "users"
   add_foreign_key "users", "registrations"
 end
