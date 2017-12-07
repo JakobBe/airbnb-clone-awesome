f27b2180cd05b925d1fc808d9dd4efa57155801b
laura hallam <laura_hallam@yahoo.co.uk>
2017-12-07 16:35:27 +0100

    styling the reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,8 +9,9 @@ class ReviewsController < ApplicationController
     @user = current_user
     @review = Review.new(reviews_params)
     @review.user_id = @user.id
+    @review.flat_id = @flat.id
     if @review.save
-    redirect_to reviews_path
+    redirect_to flat_path(@flat)
     else render :new
     end
   end
@@ -23,7 +24,7 @@ class ReviewsController < ApplicationController
   private
 
   def reviews_params
-    params.require(:review).permit(:user_id, :title, :content)
+    params.require(:review).permit(:user_id, :message, :flat_id)
   end
 
   def set_review
