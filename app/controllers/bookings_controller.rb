0cd67dc8f13fd77e8c060e53886fb1267e26a01e
Jakob Bethmann <jakob.bethmann@gmail.com>
2017-12-08 12:34:27 +0100

    Validations for booking and changes in controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,15 +6,18 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @review = Review.new
     @flat = Flat.find(params[:flat_id])
     @user = current_user
     @booking = Booking.new(bookings_params)
     @booking.user_id = @user.id
-    @booking.total_price = @booking.flat.price * @booking.nights
+
     @booking.status = "Pending"
+
     if @booking.save
+    @booking.total_price = @booking.flat.price * @booking.nights
     redirect_to bookings_path
-    else render :new
+    else render 'flats/show'
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   belongs_to :registration
   has_attachment :photo
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+
   after_initialize :init
 
     def init
